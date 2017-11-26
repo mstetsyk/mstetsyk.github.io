826c5821dd1198016b8fe1f16bef15cc27e027ac
Mykhailo Stetsyk <stetsyk1909@gmail.com>
2017-11-27 00:54:03 +0200

    continue

--- js/common.js
+++ js/common.js
@@ -1,18 +1,45 @@
 $(document).ready(function() {
 
+	$("#portfolio_grid").mixItUp();
+
+	$(".s_portfolio li").click(function() {
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	$(".popup").magnificPopup({type:"image"});
+	$(".popup_content").magnificPopup({
+		type:"inline",
+		midClick: true
+	});
+
+	$(".section_header").animated("fadeInUp", "fadeOutDown");
+
+	$(".animation_1").animated("flipInY", "fadeOutDown");
+	$(".animation_2").animated("fadeInLeft", "fadeOutDown");
+	$(".animation_3").animated("fadeInRight", "fadeOutDown");
+
+	$(".left .resume_item").animated("fadeInLeft", "fadeOutDown");
+	$(".right .resume_item").animated("fadeInRight", "fadeOutDown");
+
 	function heightDetect() {
 		$(".main_head").css("height", $(window).height());
 	};
-
 	heightDetect();
 	$(window).resize(function() {
 		heightDetect();
 	});
 
-	$(".toggle_mnu, .menu_item").click(function() {
-  	$(".sandwich").toggleClass("active");
+	$(".toggle_mnu").click(function() {
+		$(".sandwich").toggleClass("active");
 	});
-	
+
+	$(".top_mnu ul a").click(function() {
+		$(".top_mnu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+		$(".top_text").css("opacity", "1");
+	}).append("<span>");
+
 	$(".toggle_mnu").click(function() {
 		if ($(".top_mnu").is(":visible")) {
 			$(".top_text").css("opacity", "1");
